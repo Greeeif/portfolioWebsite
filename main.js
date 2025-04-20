a4e55b650acec0ffc5179242ce0ccbcfa9154afd
greeif <griffiths.alex@gmail.com>
2025-04-20 10:43:02 +0900

    adjusted the nav bar to be a collapsable sidebar instead

--- main.js
+++ main.js
@@ -20,3 +20,13 @@ const aboutMePage = document.getElementById('aboutMePage')
 aboutMePage.onclick = function() {
     window.location.href = 'aboutMe.html';
 }
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
